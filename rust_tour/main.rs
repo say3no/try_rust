02ee8ca4e6a300125714e164d063cdcde62edaa0
say3no <say3no@gmail.com>
2020-09-16 00:19:08 +0900

    tour 06

--- rust_tour/main.rs
+++ rust_tour/main.rs
@@ -27,5 +27,14 @@ fn main(){
     let bv = true;
     let t = (13, false);
     let sentence = "hello world!";
-    println!("{} {} {} {} {} {} {} ", x, a, b, c, bv, t, sentence);
+    println!("{} {} {} {} {} {} {} ", x, a, b, c, t.0, t.1, sentence);
+
+    // Tour 06
+    let a = 13u8;
+    let b = 7u32;
+    let c = a as u32 + b;
+    println!("{}", c);
+
+    let t = true;
+    println!("{}", t as u8)
 }
