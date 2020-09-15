916607dad5a318dacdf2b54cabb23f61b1c5d6d9
say3no <say3no@gmail.com>
2020-09-15 23:53:42 +0900

    hoge

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    let y = 1;
+    println!("{}", y);
+    let y = y + 1;
+    println!("{}", y);
+    let y = 333;
+    println!("{}", y);
+}
