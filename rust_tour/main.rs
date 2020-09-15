091b36d9406ee6b7e777ee9dfb8220d290ea4ff2
say3no <say3no@gmail.com>
2020-09-16 00:00:40 +0900

    tour 03

--- rust_tour/main.rs
+++ rust_tour/main.rs
@@ -1,3 +1,14 @@
 fn main(){
-    println!("hoge");
+    // x の型を推論
+    let x = 13;
+    println!("{}", x);
+
+    // x の型を指定
+    let x: f64 = 3.14159;
+    println!("{}", x);
+
+    // 宣言のあとで初期化
+    let x;
+    x =0;
+    println!("{}", x);
 }
