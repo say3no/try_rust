a25737485d1c7b98ce46258cb101980729e5c7ce
say3no <say3no@gmail.com>
2020-09-16 20:49:45 +0900

    Tour 09

--- rust_tour/main.rs
+++ rust_tour/main.rs
@@ -48,4 +48,12 @@ fn main(){
     let nums: [i32; 3] = [1,2,3];
     println!("{:?}", nums);
     println!("{}", nums[1]);
+
+    // Tour 09
+    println!("{}", add(42,13));
+}
+
+// Tour 09
+fn add(x: i32, y: i32) -> i32{
+    return x + y;
 }
