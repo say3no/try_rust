7c30b4381c91f58c120a782fedfc5b4720daae11
say3no <say3no@gmail.com>
2020-09-16 22:12:46 +0900

    Tour 19

--- rust_tour/main02.rs
+++ rust_tour/main02.rs
@@ -56,4 +56,14 @@ fn main() -> () {
         }
     }
 
+    // Tour 19
+    let mut x = 0;
+    let v = loop {
+        x += 1;
+        if x == 13 {
+            break "13 を発見";
+        }
+    };
+    println!("loop の戻り値: {}", v);
+
 }
