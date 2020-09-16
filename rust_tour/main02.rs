0ecaea0814eaae82fffbc74c95e319d0ac1317ab
say3no <say3no@gmail.com>
2020-09-16 21:14:30 +0900

    Tour 14

---
+++ rust_tour/main02.rs
@@ -0,0 +1,11 @@
+fn main() -> () {
+    // Tour 14
+    let x = 42;
+    if x < 42 {
+        println!(" 42 より小さい ");
+    } else if x == 42 {
+        println!(" 42 に等しい ");
+    } else {
+        println!(" 42 より大きい ");
+    }
+}
