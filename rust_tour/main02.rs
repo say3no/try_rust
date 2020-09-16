5648a3d98c12bb3d64e16a62be1cc9ccc2201c77
say3no <say3no@gmail.com>
2020-09-16 21:51:40 +0900

    Tour 18

--- rust_tour/main02.rs
+++ rust_tour/main02.rs
@@ -36,4 +36,24 @@ fn main() -> () {
         println!("{}",x);
     }
 
+    // Tour 18
+    let x = 42;
+    match x {
+        0 => {
+            println!("found zero");
+        }
+        1 | 2 => {
+            println!("found 1 or 2");
+        }
+        3..=9 => {
+            println!("found a number 3 to 9 inclusively");
+        }
+        matched_num @ 10..=100 => {
+            println!("found {} number between 10 to 100", matched_num);
+        }
+        _ => {
+            println!("found something else!");
+        }
+    }
+
 }
