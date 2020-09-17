91367b3201df99bddc224254d638794b85a17587
say3no <say3no@gmail.com>
2020-09-17 22:28:53 +0900

    Tour 24

--- rust_tour/main03.rs
+++ rust_tour/main03.rs
@@ -8,3 +8,10 @@ struct SeaCreature {
     legs: i32,
     weapon: String,
 }
+
+fn main() -> () {
+    // スタティックメソッドでStringインスタンスを作成する
+    let s = String::from("Hello world!");
+    // インスタンスを使ってメソッドを呼び出す
+    println!("{} is {} characters long.", s, s.len());
+}
