0883ac8a9bbb610dffa1c2ca29a6ce71f2453f2b
say3no <say3no@gmail.com>
2020-09-17 22:31:11 +0900

    hoge

--- rust_tour/main03.rs
+++ rust_tour/main03.rs
@@ -10,8 +10,14 @@ struct SeaCreature {
 }
 
 fn main() -> () {
+    // Tour 24
     // スタティックメソッドでStringインスタンスを作成する
+    // StaticMethodは　:: で呼べる
     let s = String::from("Hello world!");
     // インスタンスを使ってメソッドを呼び出す
+    // InstanceMethodは　.  で呼べる
     println!("{} is {} characters long.", s, s.len());
+
+
+
 }
