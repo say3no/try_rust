1bff1abb18d6c976d07444e2bf6765a97ad36c41
say3no <say3no@gmail.com>
2020-09-17 22:19:48 +0900

    Tour 20

--- rust_tour/main02.rs
+++ rust_tour/main02.rs
@@ -66,4 +66,33 @@ fn main() -> () {
     };
     println!("loop の戻り値: {}", v);
 
+    // Tour 20 
+    println!("関数より： {}",  example() );
+
+}
+
+// Tour 20
+fn example() -> i32 {
+    let x = 42;
+    // Rust の三項式
+    let v = if x < 42 { -1 } else { 1 };
+    println!("if より: {}", v);
+
+    let food = "ハンバーガー";
+    let result = match food {
+        "ホットドッグ" => "ホットドッグですよ",
+        // 単一の式で値を返す場合、中括弧は省略可能
+        _ => "ホットドッグではありません",
+    };
+    println!("食品の識別：　{}", result);
+
+    let v = {
+        let a = 1;
+        let b = 2;
+        a + b
+    };
+    println!("ブロックより: {}", v);
+
+    // Rust で関数の最期から値を返す観葉的な方法
+    v + 4
 }
