ea8b1dbd81b6557636aefcf1638c60c14f78eaf9
say3no <say3no@gmail.com>
2020-10-11 13:25:13 +0900

    Rename projects to the book

--- projects/guessing_game/target/debug/deps/rand-495fd87ac3a8393b.d
+++
@@ -1,9 +0,0 @@
-/Users/sanosei/github.com/try_rust/projects/guessing_game/target/debug/deps/rand-495fd87ac3a8393b.rmeta: /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/sanosei/github.com/try_rust/projects/guessing_game/target/debug/deps/librand-495fd87ac3a8393b.rlib: /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/sanosei/github.com/try_rust/projects/guessing_game/target/debug/deps/rand-495fd87ac3a8393b.d: /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/Users/sanosei/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
--- projects/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/sanosei/github.com/try_rust/projects/guessing_game/target/debug/guessing_game: /Users/sanosei/github.com/try_rust/projects/guessing_game/src/main.rs
