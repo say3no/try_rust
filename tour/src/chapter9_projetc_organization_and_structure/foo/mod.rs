55aa171e612635383e27ce349c0895fe63331117
say3no <say3no@gmail.com>
2020-10-11 13:14:07 +0900

    Tour 118 Your Own Prelude

--- tour/src/chapter9_projetc_organization_and_structure/foo/mod.rs
+++ tour/src/chapter9_projetc_organization_and_structure/foo/mod.rs
@@ -1,1 +1,9 @@
-
+// SeaCreature struct will be usable outside of our module
+pub struct SeaCreature {
+    pub animal_type: String,
+    pub name: String,
+    pub arms: i32,
+    pub legs: i32,
+    // let's keep our weapon private
+    weapon: String,
+}
